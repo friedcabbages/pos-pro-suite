927b4c54feeab60df5bc72b6679bd5c039719f31
Louis Vernando <crazyfuryy@gmail.com>
2026-01-01 19:45:05 +0700

    Add Vite JS config and Supabase local gitignore

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: true,
+    port: 8080,
+  },
+
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+  ].filter(Boolean),
+
+  resolve: {
+    alias: {
+      "@": path.resolve(process.cwd(), "src"),
+    },
+  },
+
+  build: {
+    sourcemap: false,
+  },
+
+  optimizeDeps: {
+    exclude: ["lucide-react"],
+  },
+
+  logLevel: "error",
+}));
